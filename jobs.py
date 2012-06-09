49eeb82674ff7416ddfac6cee07a57dc5c50f6d9
justzx <justzx@justzx.(none)>
2012-06-08 22:36:29 -0400

    完成了招聘信息抓取

--- jobs.py
+++ jobs.py
@@ -6,7 +6,7 @@ from lxml import html
 from datetime import datetime, timedelta
 
 # get news in last X days
-DAY = 2
+DAY = 3
 
 # url of the news page
 BASE_URL = "http://job.xidian.edu.cn/"
@@ -17,23 +17,22 @@ r = requests.get(BASE_URL)
 # override the encoding
 r.encoding = 'GBK'
 
-doc = html.document_fromstring(r.text)
+doc = html.document_fromstring(r.content)
 today = datetime.today()
 
 
 def get_today_news():
-    trs = doc.cssselect('table[width="723px"] tr')
-    for tr in trs:
-        #img_tag = tr.cssselect('img')
-        date_tag = tr.cssselect('span[class="titleTime"]')
-        #print date_tag
-        if  date_tag:
-            tr_date = date_tag[0].text_content().strip('[]')
-            #print tr_date
-            tr_timedelta = today - datetime.strptime('2012.'+tr_date, "%Y.%m.%d")
-            if tr_timedelta < timedelta(DAY):
-                link = tr.cssselect('a')[0].get('href')
-                print tr.text_content(), BASE_URL + link
+    tds = doc.cssselect('table[width="723px"] td')
+    for td in tds:
+        content_tag = td.cssselect('span[class="listTitle"]')
+        date_tag = td.cssselect('span[class="titleTime"]')
+        if  date_tag and content_tag:
+            td_date = date_tag[0].text_content().strip('[]')
+            td_timedelta = today - datetime.strptime('2012.'+td_date, "%Y.%m.%d")
+            if td_timedelta < timedelta(DAY):
+                link = td.cssselect('a')[0].get('href')
+                title = td.cssselect('a')[0].get('title')
+                print datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d') , title, BASE_URL + link
 
 if __name__ == '__main__':
     get_today_news()
