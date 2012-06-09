08a49626526bf90c763ae7382eb86dd8aa9196b4
justzx <justzx@justzx.(none)>
2012-06-08 23:23:10 -0400

    重定向到html

--- jobs.py
+++ jobs.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import popen2
 import requests
 from lxml import html
 from datetime import datetime, timedelta
-
 # get news in last X days
 DAY = 3
-
 # url of the news page
 BASE_URL = "http://job.xidian.edu.cn/"
 
@@ -22,6 +20,9 @@ today = datetime.today()
 
 
 def get_today_news():
+    fin,fout = popen2.popen2("tee -a index.html")
+    fout.write("<html>")
+    fout.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=gb2312\">")
     tds = doc.cssselect('table[width="723px"] td')
     for td in tds:
         content_tag = td.cssselect('span[class="listTitle"]')
@@ -32,7 +33,16 @@ def get_today_news():
             if td_timedelta < timedelta(DAY):
                 link = td.cssselect('a')[0].get('href')
                 title = td.cssselect('a')[0].get('title')
-                print datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d') , title, BASE_URL + link
-
+                #print datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d') , title, BASE_URL + link
+                fout.write("<p>")                
+                fout.write(str(datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d')))     
+                fout.write("<a href=\"")
+                fout.write(str(BASE_URL + link))
+                fout.write("\">")
+                fout.write("title")
+                fout.write("</a>")
+                fout.write("</p>")
+    fout.write("</html>")
+    fout.close()      
 if __name__ == '__main__':
     get_today_news()
