4d46a7a10b7165df1daf578ae45f2c78a5dfec13
justzx <justzx@justzx.(none)>
2012-06-11 22:47:21 -0400

    完成了招聘信息的抓取

--- jobs.py
+++ jobs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import popen2
+import urllib2 
+from BeautifulSoup import BeautifulSoup
 import os
 import requests
 from lxml import html
 from datetime import datetime, timedelta
 # get news in last X days
-DAY = 3
+DAY = 4
 # url of the news page
 BASE_URL = "http://job.xidian.edu.cn/"
 
@@ -21,14 +23,23 @@ today = datetime.today()
 
 
 def get_today_news():
-    os.system("/bin/echo "" >index.html") 
-    fin,fout = popen2.popen2("tee -a index.html")
+    homedir = os.getcwd()
+    os.system("/bin/echo "" > cache/index.html") 
+    fin,fout = popen2.popen2("tee -a cache/index.html")
     fout.write("<html>")
     fout.write("<head>")
     fout.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=gb2312\">")
-    fout.write("<link rel=\"Stylesheet\" type=\"text/css\" href=\"style.css\">")
+    fout.write("<link rel=\"Stylesheet\" type=\"text/css\" href=\"css/style.css\">")
+    #fout.write("<script src=\"goto.js\"/>")
+    fout.write("<script src=\"script/jquery-1.7.1.min.js\"></script>")
+    fout.write("<script src=\"script/jquery.mobile-1.1.0.min.js\"></script>")
+    fout.write("<script>function GetContent(i){var content=$('#news_'+i).attr('rel');localStorage.content=content;}</script>")
+    
     fout.write("</head>")
+    fout.write("<body>")
     tds = doc.cssselect('table[width="723px"] td')
+    #3global newsnum
+    newsnum = 0
     for td in tds:
         content_tag = td.cssselect('span[class="listTitle"]')
         date_tag = td.cssselect('span[class="titleTime"]')
@@ -39,16 +50,50 @@ def get_today_news():
                 link = td.cssselect('a')[0].get('href')
                 title = td.cssselect('a')[0].get('title')
                 #print datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d') , title, BASE_URL + link
-                fout.write("<p><h3>")                
-                fout.write(str(datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d')))     
-                fout.write("<a href=\"")
-                fout.write(str(BASE_URL + link))
-                fout.write("\">")
-                #print title.encode('gb2312')
-                #fout.write(title.decode('GBK').encode('gb2312'))
+                web=urllib2.urlopen(str(BASE_URL + link))
+                soup = BeautifulSoup(web)
+                #print web
+                content = soup.body.div.extract()
+                file=open('cache/news_'+str(newsnum) + '.html','w+') 
+                head = BeautifulSoup(open(homedir +  "/cache/construction/head.html"))
+                bodyend = BeautifulSoup(open(homedir +  "/cache/construction/bodyend.html"))
+                print >> file,head
+                print >> file,"<body>"
+                print >> file,content
+                print >> file,bodyend
+                file.close()  
+                fout.write("<div id=\"news_")                
+                fout.write(str(newsnum))                
+                fout.write("\">")                
+                fout.write("<a href=")                
+                fout.write("\"news_")
+                fout.write(str(newsnum))
+                fout.write(".html\">")                
                 fout.write(title.encode('gb2312'))
                 fout.write("</a>")
-                fout.write("</h3></p>")
+                newsnum  = newsnum + 1
+                fout.write("<span>")
+                fout.write(str(datetime.strptime('2012.'+td_date, "%Y.%m.%d").strftime('%Y-%m-%d'))) 
+                fout.write("</span>")
+                fout.write("</div>")                
+                 
+    
+                #fout.write("<a onclick=\"goto(str(BASE_URL + link))\"")
+		#fout.write("<a href="ddd.html?#id_1">");
+                #fout.write(str(BASE_URL + link))
+                #fout.write("\">")
+                
+
+
+
+
+
+                #print title.encode('gb2312')
+                #fout.write(title.decode('GBK').encode('gb2312'))
+                #fout.write(title.encode('gb2312'))
+                #fout.write("</a>")
+                #fout.write("</h3></p>")
+    fout.write("</body>")
     fout.write("</html>")
     fout.close()      
 if __name__ == '__main__':
